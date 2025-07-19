37e8295e3bfd622d4c750d7ea383781b155843fb
MarLatte <86508134+marlatte@users.noreply.github.com>
2025-07-19 01:30:14 -0400

    Add husky pre-commit workflow

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,24 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
-import { defineConfig } from 'eslint/config';
-import vitest from '@vitest/eslint-plugin';
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import vitest from "@vitest/eslint-plugin";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import stylistic from "@stylistic/eslint-plugin";
 
 export default defineConfig([
+  eslintConfigPrettier,
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    plugins: { js },
-    extends: ['js/recommended'],
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js, "@stylistic": stylistic },
+    extends: ["js/recommended"],
   },
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     languageOptions: { globals: globals.browser },
   },
   {
-    files: ['**/*.test.{js,mjs,cjs,ts,mts,cts}'],
+    files: ["**/*.test.{js,mjs,cjs,ts,mts,cts}"],
     ...vitest.configs.recommended,
   },
   tseslint.configs.recommended,
